7c707966ca2e4d6edb0cedec0d3cdd8a50158767
Savenok <Romasav2017@gmail.com>
2022-09-11 17:29:16 +0300

    26

--- Program.cs
+++ Program.cs
@@ -1,61 +1,13 @@
-﻿// ЗАчада 24
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
 Console.Clear();
 Console.Write("Введите число: ");
-int b = int.Parse(Console.ReadLine());
-int sum = 0;
-bool a = true;
-while( a == true)
+string cc = Console.ReadLine();
+int Countnumbers(string count)
 {
-    if ( b == 0 ) 
-    {
-        Console.WriteLine("Это число не подходит");
-        Console.Write("Введите число: ");        
-        int b3 = int.Parse(Console.ReadLine());
-        if (b3 == 0)
-        {
-            Console.WriteLine("Это число не подходит");
-            Console.Write("Больше проверять не буду, устала)");        
-        }
-        else if( b < 0 )
-        {
-            Console.Write("Это число отрицательное, но можем сделать сумму положительных чисел: ");
-            for(int d = 0 ; d <= -b3 ; d++)
-            {
-                sum = sum + d; 
-            }
-            Console.Write($"{b3} -> {sum}");
-            a = false;
-        }
-        else
-        {
-            
-            for(int d = 0 ; d <= b3 ; d++)
-        {
-            sum = sum + d; 
-        }
-        Console.Write($"{b3} -> {sum}");
-        a = false; 
-        }
-    }
-    else if (b < 0)
-    {
-        Console.Write("Это число отрицательное, но можем сделать сумму положительных чисел: ");        
-        for(int x = 0 ; x <= -b ; x++)
-        {
-            sum = sum + x; 
-        }
-        Console.Write($"{b} -> {sum}");
-        a = false;
-    }
-    else
-    {
-        for(int i = 0 ; i <= b ; i++)
-        {
-            sum = sum + i; 
-        }
-        Console.Write($"{b} -> {sum}");
-        a = false;
-    }
-    a = false;
+    int a = cc.Length;
+    return (int)cc.Length;
 }
-
+Console.Write($"{cc} -> {Countnumbers(cc)}");
